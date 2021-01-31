3cdf00ffc26b9d1aea7c1aafeee016bbeb096927
Maciej <maciej.zelazo@protonmail.com>
2021-01-31 16:21:19 +0100

    changed colors od design

---
+++ app.js
@@ -0,0 +1,13 @@
+import 'bootstrap';
+import AOS from 'aos';
+
+AOS.init({
+    duration: 500,
+    // easing: 'cubic-bezier(.91, .11, .19, .92)'
+    // once: true
+});
+
+// $('.ask-animation').each(function (i) {
+//     $(this).addClass('aos-init aos-animate');
+//     $(this).attr('data-aos', 'fade-in-left');
+// });
