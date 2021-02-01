122982577e41e9b124518aad7a762bcbbf9f07da
Maciej <maciej.zelazo@protonmail.com>
2021-02-01 01:14:52 +0100

    changed colors

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -204,7 +204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40501" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36125" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
