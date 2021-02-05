216af554d5128751e04d76d25dad63fc5c533aa3
Maciej <maciej.zelazo@protonmail.com>
2021-02-05 02:54:27 +0100

    changed colors in backend

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -21845,7 +21845,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35989" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35263" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
