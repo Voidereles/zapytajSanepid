4f3556c37d2fe9cd2541c41ea8e49b54ccb1a86a
Maciej <maciej.zelazo@protonmail.com>
2021-02-07 11:11:30 +0100

    preload fonts no

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -21894,7 +21894,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46771" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45875" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
