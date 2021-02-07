d50ff121dabe21036ff047617f018e96faee0476
Maciej <maciej.zelazo@protonmail.com>
2021-02-07 12:16:57 +0100

    twitter change

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -21898,7 +21898,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38331" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43223" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
