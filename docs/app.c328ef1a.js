31e09db3b7f87ae34df02acc3ea84d8d19e18f85
Maciej <maciej.zelazo@protonmail.com>
2021-02-07 11:41:09 +0100

    border to 1

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -21894,7 +21894,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45121" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36471" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
